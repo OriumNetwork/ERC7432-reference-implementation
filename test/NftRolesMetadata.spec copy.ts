86e65235e7b4a8863b92b8335a7f638e0d94739b
Daniel Lima <daniel.lima@orium.network>
2023-07-19 15:54:17 -0300

    role metadata

---
+++ test/NftRolesMetadata.spec copy.ts
@@ -0,0 +1,53 @@
+import hre, { ethers } from 'hardhat'
+import { Contract } from 'ethers'
+import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers'
+import { expect } from 'chai'
+import { randomHash } from './utils'
+
+const { HashZero, AddressZero } = ethers.constants
+const ONE_DAY = 60 * 60 * 24
+
+describe('Nfts Roles Metadata', () => {
+  let nftRolesMetadata: Contract
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  let deployer: SignerWithAddress
+
+  const role = randomHash()
+  const name = 'role name'
+  const description = 'role description'
+  const metadata = randomHash()
+
+  before(async function () {
+    // prettier-ignore
+    [deployer] = await ethers.getSigners()
+  })
+
+  beforeEach(async () => {
+    const NftRolesMetadataFactory = await ethers.getContractFactory('NftRolesMetadata')
+    nftRolesMetadata = await NftRolesMetadataFactory.deploy()
+  })
+
+  describe('Nft Roles', async () => {
+    describe('Set role metadata', async () => {
+      it('should set role metadata', async () => {
+        await expect(nftRolesMetadata.setRoleMetadata(role, name, description, metadata)).to.not.be.reverted
+      })
+    })
+
+    describe('Get role metadata', async () => {
+      beforeEach(async () => {
+        await nftRolesMetadata.setRoleMetadata(role, name, description, metadata)
+      })
+      it('should get role name', async () => {
+        expect(await nftRolesMetadata.roleName(role)).to.equal(name)
+      })
+      it('should get role description', async () => {
+        expect(await nftRolesMetadata.roleDescription(role)).to.equal(description)
+      })
+      it('should get role metadata', async () => {
+        expect(await nftRolesMetadata.roleMetadata(role)).to.equal(metadata)
+      })
+    })
+  })
+})
