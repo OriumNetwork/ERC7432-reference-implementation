ee5d8966483e09b55d6314987acd80721cf9b92a
Daniel Lima <daniel.lima@orium.network>
2023-08-03 16:27:48 -0300

    skip mocks folder on solidity coverage

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -23,7 +23,7 @@ module.exports = {
   },
   gasReporter: {
     enabled: true,
-    excludeContracts: ['contracts/test'],
+    excludeContracts: ['contracts/mocks'],
     gasPrice: 100,
     token: 'MATIC',
     currency: 'USD',
