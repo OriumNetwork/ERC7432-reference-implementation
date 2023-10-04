495b412f4b3d6fe37f124d314302e754ba413a65
Daniel Lima <daniel.lima@orium.network>
2023-10-04 16:39:54 -0300

    Update interface to use structs and remove tokenId level approvals

--- contracts/ERC7432/ERC7432.sol
+++ contracts/ERC7432/ERC7432.sol
@@ -23,133 +23,115 @@ contract ERC7432 is IERC7432 {
         _;
     }
 
-    modifier onlyApproved(address _tokenAddress, uint256 _tokenId, address _account) {
-        require(
-            _isRoleApproved(_tokenAddress, _tokenId, _account, msg.sender),
-            "ERC7432: sender must be approved"
-        );
+    modifier onlyApproved(
+        address _tokenAddress,
+        uint256 _tokenId,
+        address _account
+    ) {
+        require(isRoleApprovedForAll(_tokenAddress, _account, msg.sender), "ERC7432: sender must be approved");
         _;
     }
 
-    function grantRole(
-        bytes32 _role,
-        address _tokenAddress,
-        uint256 _tokenId,
-        address _grantee,
-        uint64 _expirationDate,
-        bool _revocable,
+    function grantRoleFrom(
+        RoleAssignment calldata _roleAssignment,
         bytes calldata _data
-    ) external {
-        _grantRole(_role, _tokenAddress, _tokenId, msg.sender, _grantee, _expirationDate, _revocable, _data);
+    ) external override onlyApproved(_roleAssignment.tokenAddress, _roleAssignment.tokenId, _roleAssignment.grantor) {
+        _grantRole(_roleAssignment, false, _data);
     }
 
-    function grantRoleFrom(
-        bytes32 _role,
-        address _tokenAddress,
-        uint256 _tokenId,
-        address _grantor,
-        address _grantee,
-        uint64 _expirationDate,
-        bool _revocable,
+    function grantRevocableRoleFrom(
+        RoleAssignment calldata _roleAssignment,
         bytes calldata _data
-    ) external override onlyApproved(_tokenAddress, _tokenId, _grantor) {
-        _grantRole(_role, _tokenAddress, _tokenId, _grantor, _grantee, _expirationDate, _revocable, _data);
+    ) external override onlyApproved(_roleAssignment.tokenAddress, _roleAssignment.tokenId, _roleAssignment.grantor) {
+        _grantRole(_roleAssignment, true, _data);
     }
 
     function _grantRole(
-        bytes32 _role,
-        address _tokenAddress,
-        uint256 _tokenId,
-        address _grantor,
-        address _grantee,
-        uint64 _expirationDate,
+        RoleAssignment memory _roleAssignment,
         bool _revocable,
         bytes calldata _data
-    ) internal validExpirationDate(_expirationDate) {
-        roleAssignments[_grantor][_grantee][_tokenAddress][_tokenId][_role] = RoleData(_expirationDate, _revocable, _data);
-        latestGrantees[_grantor][_tokenAddress][_tokenId][_role] = _grantee;
-        emit RoleGranted(_role, _tokenAddress, _tokenId, _grantor, _grantee, _expirationDate, _revocable, _data);
+    ) internal validExpirationDate(_roleAssignment.expirationDate) {
+        roleAssignments[_roleAssignment.grantor][_roleAssignment.grantee][_roleAssignment.tokenAddress][
+            _roleAssignment.tokenId
+        ][_roleAssignment.role] = RoleData(_roleAssignment.expirationDate, _revocable, _data);
+        latestGrantees[_roleAssignment.grantor][_roleAssignment.tokenAddress][_roleAssignment.tokenId][
+            _roleAssignment.role
+        ] = _roleAssignment.grantee;
+        emit RoleGranted(_roleAssignment, _revocable, _data);
     }
 
-    function revokeRole(bytes32 _role, address _tokenAddress, uint256 _tokenId, address _grantee) external {
-        _revokeRole(_role, _tokenAddress, _tokenId, msg.sender, _grantee, msg.sender);
+    function revokeRoleFrom(RoleAssignment calldata _roleAssignment) external override {
+        address _caller = _getApprovedCaller(
+            _roleAssignment.tokenAddress,
+            _roleAssignment.grantor,
+            _roleAssignment.grantee
+        );
+        _revokeRole(_roleAssignment, _caller);
     }
 
-    function revokeRoleFrom(
-        bytes32 _role,
+    function _getApprovedCaller(
         address _tokenAddress,
-        uint256 _tokenId,
         address _revoker,
         address _grantee
-    ) external override {
-        address _caller = _getApprovedCaller(_tokenAddress, _tokenId, _revoker, _grantee);
-        _revokeRole(_role, _tokenAddress, _tokenId, _revoker, _grantee, _caller);
-    }
-
-    function _getApprovedCaller(address _tokenAddress, uint256 _tokenId, address _revoker, address _grantee) internal view returns (address) {
-        if(_isRoleApproved(_tokenAddress, _tokenId, _grantee, msg.sender)){
+    ) internal view returns (address) {
+        if (isRoleApprovedForAll(_tokenAddress, _grantee, msg.sender)) {
             return _grantee;
-        } else if(_isRoleApproved(_tokenAddress, _tokenId, _revoker, msg.sender)){
+        } else if (isRoleApprovedForAll(_tokenAddress, _revoker, msg.sender)) {
             return _revoker;
         } else {
             revert("ERC7432: sender must be approved");
         }
     }
 
-    function _revokeRole(
-        bytes32 _role,
-        address _tokenAddress,
-        uint256 _tokenId,
-        address _revoker,
-        address _grantee,
-        address _caller
-    ) internal {
-        bool _isRevocable = roleAssignments[_revoker][_grantee][_tokenAddress][_tokenId][_role].revocable;
-        require(_isRevocable || _caller == _grantee, "ERC7432: Role is not revocable or caller is not the grantee");
-        delete roleAssignments[_revoker][_grantee][_tokenAddress][_tokenId][_role];
-        delete latestGrantees[_revoker][_tokenAddress][_tokenId][_role];
-        emit RoleRevoked(_role, _tokenAddress, _tokenId, _revoker, _grantee);
+    function _revokeRole(RoleAssignment calldata _roleAssignment, address _caller) internal {
+        bool _isRevocable = roleAssignments[_roleAssignment.grantor][_roleAssignment.grantee][
+            _roleAssignment.tokenAddress
+        ][_roleAssignment.tokenId][_roleAssignment.role].revocable;
+        require(
+            _isRevocable || _caller == _roleAssignment.grantee,
+            "ERC7432: Role is not revocable or caller is not the grantee"
+        );
+        delete roleAssignments[_roleAssignment.grantor][_roleAssignment.grantee][_roleAssignment.tokenAddress][
+            _roleAssignment.tokenId
+        ][_roleAssignment.role];
+        delete latestGrantees[_roleAssignment.grantor][_roleAssignment.tokenAddress][_roleAssignment.tokenId][
+            _roleAssignment.role
+        ];
+        emit RoleRevoked(_roleAssignment);
     }
 
-    function hasRole(
-        bytes32 _role,
-        address _tokenAddress,
-        uint256 _tokenId,
-        address _grantor,
-        address _grantee
-    ) external view returns (bool) {
-        return roleAssignments[_grantor][_grantee][_tokenAddress][_tokenId][_role].expirationDate > block.timestamp;
+    function hasRole(RoleAssignment calldata _roleAssignment) external view returns (bool) {
+        return
+            roleAssignments[_roleAssignment.grantor][_roleAssignment.grantee][_roleAssignment.tokenAddress][
+                _roleAssignment.tokenId
+            ][_roleAssignment.role].expirationDate > block.timestamp;
     }
 
-    function hasUniqueRole(
-        bytes32 _role,
-        address _tokenAddress,
-        uint256 _tokenId,
-        address _grantor,
-        address _grantee
-    ) external view returns (bool) {
-        return latestGrantees[_grantor][_tokenAddress][_tokenId][_role] == _grantee && roleAssignments[_grantor][_grantee][_tokenAddress][_tokenId][_role].expirationDate > block.timestamp;
+    function hasUniqueRole(RoleAssignment calldata _roleAssignment) external view returns (bool) {
+        return
+            latestGrantees[_roleAssignment.grantor][_roleAssignment.tokenAddress][_roleAssignment.tokenId][
+                _roleAssignment.role
+            ] ==
+            _roleAssignment.grantee &&
+            roleAssignments[_roleAssignment.grantor][_roleAssignment.grantee][_roleAssignment.tokenAddress][
+                _roleAssignment.tokenId
+            ][_roleAssignment.role].expirationDate >
+            block.timestamp;
     }
 
-    function roleData(
-        bytes32 _role,
-        address _tokenAddress,
-        uint256 _tokenId,
-        address _grantor,
-        address _grantee
-    ) external view returns (bytes memory data_) {
-        RoleData memory _roleData = roleAssignments[_grantor][_grantee][_tokenAddress][_tokenId][_role];
+    function roleData(RoleAssignment calldata _roleAssignment) external view returns (bytes memory data_) {
+        RoleData memory _roleData = roleAssignments[_roleAssignment.grantor][_roleAssignment.grantee][
+            _roleAssignment.tokenAddress
+        ][_roleAssignment.tokenId][_roleAssignment.role];
         return (_roleData.data);
     }
 
     function roleExpirationDate(
-        bytes32 _role,
-        address _tokenAddress,
-        uint256 _tokenId,
-        address _grantor,
-        address _grantee
+        RoleAssignment calldata _roleAssignment
     ) external view returns (uint64 expirationDate_) {
-        RoleData memory _roleData = roleAssignments[_grantor][_grantee][_tokenAddress][_tokenId][_role];
+        RoleData memory _roleData = roleAssignments[_roleAssignment.grantor][_roleAssignment.grantee][
+            _roleAssignment.tokenAddress
+        ][_roleAssignment.tokenId][_roleAssignment.role];
         return (_roleData.expirationDate);
     }
 
@@ -157,23 +139,9 @@ contract ERC7432 is IERC7432 {
         return interfaceId == type(IERC7432).interfaceId;
     }
 
-    function setRoleApprovalForAll(
-        address _tokenAddress,
-        address _operator,
-        bool _isApproved
-    ) external override {
+    function setRoleApprovalForAll(address _tokenAddress, address _operator, bool _isApproved) external override {
         tokenApprovals[msg.sender][_tokenAddress][_operator] = _isApproved;
         emit RoleApprovalForAll(_tokenAddress, _operator, _isApproved);
-    }
-
-    function approveRole(
-        address _tokenAddress,
-        uint256 _tokenId,
-        address _operator,
-        bool _approved
-    ) external override {
-        tokenIdApprovals[msg.sender][_tokenAddress][_tokenId][_operator] = _approved;
-        emit RoleApproval(_tokenAddress, _tokenId, _operator, _approved);
     }
 
     function isRoleApprovedForAll(
@@ -184,21 +152,12 @@ contract ERC7432 is IERC7432 {
         return tokenApprovals[_grantor][_tokenAddress][_operator];
     }
 
-    function getApprovedRole(
+    function lastGrantee(
+        address _grantor,
         address _tokenAddress,
         uint256 _tokenId,
-        address _grantor,
-        address _operator
-    ) public view override returns (bool) {
-        return tokenIdApprovals[_grantor][_tokenAddress][_tokenId][_operator];
-    }
-
-    function _isRoleApproved(
-        address _tokenAddress,
-        uint256 _tokenId,
-        address _grantor,
-        address _operator
-    ) internal view returns (bool) {
-        return isRoleApprovedForAll(_tokenAddress, _grantor, _operator) || getApprovedRole(_tokenAddress, _tokenId, _grantor, _operator);
+        bytes32 _role
+    ) public view override returns (address) {
+        return latestGrantees[_grantor][_tokenAddress][_tokenId][_role];
     }
 }
--- contracts/ERC7432/interfaces/IERC7432.sol
+++ contracts/ERC7432/interfaces/IERC7432.sol
@@ -14,209 +14,72 @@ interface IERC7432 is IERC165 {
         bytes data;
     }
 
+    struct RoleAssignment {
+        bytes32 role;
+        address tokenAddress;
+        uint256 tokenId;
+        address grantor;
+        address grantee;
+        uint64 expirationDate;
+    }
+
     /** Events **/
 
     /// @notice Emitted when a role is granted.
-    /// @param _role The role identifier.
-    /// @param _tokenAddress The token address.
-    /// @param _tokenId The token identifier.
-    /// @param _grantor The user assigning the role.
-    /// @param _grantee The user receiving the role.
-    /// @param _expirationDate The expiration date of the role.
+    /// @param _roleAssignment The role assignment data.
     /// @param _revocable Whether the role is revocable or not.
-    /// @param _data Any additional data about the role.
-    event RoleGranted(
-        bytes32 indexed _role,
-        address indexed _tokenAddress,
-        uint256 indexed _tokenId,
-        address _grantor,
-        address _grantee,
-        uint64 _expirationDate,
-        bool _revocable,
-        bytes _data
-    );
+    /// @param _data The custom data of the role assignment.
+    event RoleGranted(RoleAssignment _roleAssignment, bool _revocable, bytes _data);
 
     /// @notice Emitted when a role is revoked.
-    /// @param _role The role identifier.
-    /// @param _tokenAddress The token address.
-    /// @param _tokenId The token identifier.
-    /// @param _revoker The user revoking the role.
-    /// @param _grantee The user that receives the role revocation.
-    event RoleRevoked(
-        bytes32 indexed _role,
-        address indexed _tokenAddress,
-        uint256 indexed _tokenId,
-        address _revoker,
-        address _grantee
-    );
+    /// @param _roleAssignment The role assignment data.
+    event RoleRevoked(RoleAssignment _roleAssignment);
 
     /// @notice Emitted when a user is approved to manage any role on behalf of another user.
     /// @param _tokenAddress The token address.
     /// @param _operator The user approved to grant and revoke roles.
     /// @param _isApproved The approval status.
-    event RoleApprovalForAll(
-        address indexed _tokenAddress,
-        address indexed _operator,
-        bool _isApproved
-    );
-
-    /// @notice Emitted when a user is approved to manage the roles of an NFT on behalf of another user.
-    /// @param _tokenAddress The token address.
-    /// @param _tokenId The token identifier.
-    /// @param _operator The user approved to grant and revoke roles.
-    /// @param _isApproved The approval status.
-    event RoleApproval(
-        address indexed _tokenAddress,
-        uint256 indexed _tokenId,
-        address _operator,
-        bool _isApproved
-    );
+    event RoleApprovalForAll(address indexed _tokenAddress, address indexed _operator, bool _isApproved);
 
     /** External Functions **/
 
-    /// @notice Grants a role to a user.
-    /// @param _role The role identifier.
-    /// @param _tokenAddress The token address.
-    /// @param _tokenId The token identifier.
-    /// @param _grantee The user receiving the role.
-    /// @param _expirationDate The expiration date of the role.
-    /// @param _revocable Whether the role is revocable or not.
-    /// @param _data Any additional data about the role.
-    function grantRole(
-        bytes32 _role,
-        address _tokenAddress,
-        uint256 _tokenId,
-        address _grantee,
-        uint64 _expirationDate,
-        bool _revocable,
-        bytes calldata _data
-    ) external;
+    /// @notice Grants a role by the grantor or an approved operator.
+    /// @param _roleAssignment The role assignment data.
+    /// @param _data The custom data of the role assignment.
+    function grantRoleFrom(RoleAssignment calldata _roleAssignment, bytes calldata _data) external;
 
-    /// @notice Revokes a role from a user.
-    /// @param _role The role identifier.
-    /// @param _tokenAddress The token address.
-    /// @param _tokenId The token identifier.
-    /// @param _grantee The user that receives the role revocation.
-    function revokeRole(
-        bytes32 _role,
-        address _tokenAddress,
-        uint256 _tokenId,
-        address _grantee
-    ) external;
-
-    /// @notice Grants a role on behalf of a user.
-    /// @param _role The role identifier.
-    /// @param _tokenAddress The token address.
-    /// @param _tokenId The token identifier.
-    /// @param _grantor The user assigning the role.
-    /// @param _grantee The user that receives the role.
-    /// @param _expirationDate The expiration date of the role.
-    /// @param _revocable Whether the role is revocable or not.
-    /// @param _data Any additional data about the role.
-    function grantRoleFrom(
-        bytes32 _role,
-        address _tokenAddress,
-        uint256 _tokenId,
-        address _grantor,
-        address _grantee,
-        uint64 _expirationDate,
-        bool _revocable,
-        bytes calldata _data
-    ) external;
+    /// @notice Grants a revocable role by the grantor or an approved operator.
+    /// @param _roleAssignment The role assignment data.
+    /// @param _data The custom data of the role assignment.
+    function grantRevocableRoleFrom(RoleAssignment calldata _roleAssignment, bytes calldata _data) external;
 
     /// @notice Revokes a role on behalf of a user.
-    /// @param _role The role identifier.
-    /// @param _tokenAddress The token address.
-    /// @param _tokenId The token identifier.
-    /// @param _revoker The user revoking the role.
-    /// @param _grantee The user that receives the role revocation.
-    function revokeRoleFrom(
-        bytes32 _role,
-        address _tokenAddress,
-        uint256 _tokenId,
-        address _revoker,
-        address _grantee
-    ) external;
+    /// @param _roleAssignment The role assignment data.
+    function revokeRoleFrom(RoleAssignment calldata _roleAssignment) external;
 
     /// @notice Approves operator to grant and revoke any roles on behalf of another user.
     /// @param _tokenAddress The token address.
     /// @param _operator The user approved to grant and revoke roles.
     /// @param _approved The approval status.
-    function setRoleApprovalForAll(
-        address _tokenAddress,
-        address _operator,
-        bool _approved
-    ) external;
-
-    /// @notice Approves operator to grant and revoke roles of an NFT on behalf of another user.
-    /// @param _tokenAddress The token address.
-    /// @param _tokenId The token identifier.
-    /// @param _operator The user approved to grant and revoke roles.
-    /// @param _approved The approval status.
-    function approveRole(
-        address _tokenAddress,
-        uint256 _tokenId,
-        address _operator,
-        bool _approved
-    ) external;
+    function setRoleApprovalForAll(address _tokenAddress, address _operator, bool _approved) external;
 
     /** View Functions **/
 
     /// @notice Checks if a user has a role.
-    /// @param _role The role identifier.
-    /// @param _tokenAddress The token address.
-    /// @param _tokenId The token identifier.
-    /// @param _grantor The user that assigned the role.
-    /// @param _grantee The user that received the role.
-    function hasRole(
-        bytes32 _role,
-        address _tokenAddress,
-        uint256 _tokenId,
-        address _grantor,
-        address _grantee
-    ) external view returns (bool);
+    /// @param _roleAssignment The role assignment data.
+    function hasRole(RoleAssignment calldata _roleAssignment) external view returns (bool);
 
     /// @notice Checks if a user has a unique role.
-    /// @param _role The role identifier.
-    /// @param _tokenAddress The token address.
-    /// @param _tokenId The token identifier.
-    /// @param _grantor The user that assigned the role.
-    /// @param _grantee The user that received the role.
-    function hasUniqueRole(
-        bytes32 _role,
-        address _tokenAddress,
-        uint256 _tokenId,
-        address _grantor,
-        address _grantee
-    ) external view returns (bool);
+    /// @param _roleAssignment The role assignment data.
+    function hasUniqueRole(RoleAssignment calldata _roleAssignment) external view returns (bool);
 
     /// @notice Returns the custom data of a role assignment.
-    /// @param _role The role identifier.
-    /// @param _tokenAddress The token address.
-    /// @param _tokenId The token identifier.
-    /// @param _grantor The user that assigned the role.
-    /// @param _grantee The user that received the role.
-    function roleData(
-        bytes32 _role,
-        address _tokenAddress,
-        uint256 _tokenId,
-        address _grantor,
-        address _grantee
-    ) external view returns (bytes memory data_);
+    /// @param _roleAssignment The role assignment data.
+    function roleData(RoleAssignment calldata _roleAssignment) external view returns (bytes memory data_);
 
     /// @notice Returns the expiration date of a role assignment.
-    /// @param _role The role identifier.
-    /// @param _tokenAddress The token address.
-    /// @param _tokenId The token identifier.
-    /// @param _grantor The user that assigned the role.
-    /// @param _grantee The user that received the role.
-    function roleExpirationDate(
-        bytes32 _role,
-        address _tokenAddress,
-        uint256 _tokenId,
-        address _grantor,
-        address _grantee
-    ) external view returns (uint64 expirationDate_);
+    /// @param _roleAssignment The role assignment data.
+    function roleExpirationDate(RoleAssignment calldata _roleAssignment) external view returns (uint64 expirationDate_);
 
     /// @notice Checks if the grantor approved the operator for all NFTs.
     /// @param _tokenAddress The token address.
@@ -228,15 +91,16 @@ interface IERC7432 is IERC165 {
         address _operator
     ) external view returns (bool);
 
-    /// @notice Checks if the grantor approved the operator for a specific NFT.
+
+    /// @notice Returns the last grantee of a role.
+    /// @param _grantor The user that granted the role.
     /// @param _tokenAddress The token address.
-    /// @param _tokenId The token identifier.
-    /// @param _grantor The user that approved the operator.
-    /// @param _operator The user approved to grant and revoke roles.
-    function getApprovedRole(
+    /// @param _tokenId The token ID.
+    /// @param _role The role.
+    function lastGrantee(
+        address _grantor,
         address _tokenAddress,
         uint256 _tokenId,
-        address _grantor,
-        address _operator
-    ) external view returns (bool);
+        bytes32 _role
+    ) external view returns (address);
 }
