dce13b4bb03a3a85145d8a04be313f873f462208
Daniel Lima <daniel.lima@orium.network>
2023-10-05 14:03:04 -0300

    Update interface ID

--- contracts/ERC7432/interfaces/IERC7432.sol
+++ contracts/ERC7432/interfaces/IERC7432.sol
@@ -6,7 +6,7 @@ import { IERC165 } from "@openzeppelin/contracts/utils/introspection/IERC165.sol
 
 /// @title ERC-7432 Non-Fungible Token Roles
 /// @dev See https://eips.ethereum.org/EIPS/eip-7432
-/// Note: the ERC-165 identifier for this interface is 0x17ef8677.
+/// Note: the ERC-165 identifier for this interface is 0x04984ac8.
 interface IERC7432 is IERC165 {
     struct RoleData {
         uint64 expirationDate;
